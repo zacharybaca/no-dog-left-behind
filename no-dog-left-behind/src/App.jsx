c5cf02876b61c62722c78b0828bd0c10393d18da
zacharybaca <zachary.baca@msn.com>
2025-06-11 18:23:17 -0500

    added some design files

--- no-dog-left-behind/src/App.jsx
+++ no-dog-left-behind/src/App.jsx
@@ -1,16 +1,24 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
 import Login from './components/Login/Login.jsx';
+import AppLogo from './assets/no-dog-left-behind-logo.png';
 
 function App() {
 
 
   return (
     <div id="app-container">
-
+      {/* This Section Will Show Up With Each Rendered Component */}
+      <img src={AppLogo} alt="application-logo" className="application-logo" />
       <HeroSection />
-      <Login />
+      <hr id="horizontal-ruler-default" />
+
+      {/* Routes and Their Rendered Components */}
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+      </Routes>
 
     </div>
   )
--- no-dog-left-behind/src/components/Login/Login.jsx
+++ no-dog-left-behind/src/components/Login/Login.jsx
@@ -5,19 +5,13 @@ const Login = () => {
 
     return (
         <div className="login-container">
-
-
-
-
-            <img src={PawPrintImage} alt="Paw Print Background" className="login-bg" />
-
             <form className="login-form">
+                <img src={PawPrintImage} alt="Paw Print Background" className="login-bg" />
                 <h2>Welcome Back</h2>
                 <input type="text" placeholder="Username" required />
                 <input type="password" placeholder="Password" required />
                 <button type="submit">Log In</button>
             </form>
-
         </div>
     )
 }

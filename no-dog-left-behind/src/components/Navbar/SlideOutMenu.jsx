c04317dc425369a2ddf9b2d858591da6df6cbfa7
Zachary Baca <zachary.baca@msn.com>
2025-06-12 10:48:35 -0500

    Update SlideOutMenu.jsx

--- no-dog-left-behind/src/components/Navbar/SlideOutMenu.jsx
+++ no-dog-left-behind/src/components/Navbar/SlideOutMenu.jsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import './slide-out-menu.css';
 
-const SlideOutMenu = ({ isOpen }) => {
+const SlideOutMenu = ({ isOpen, onClose }) => {
   return (
-    <div className={`slideout-menu ${isOpen ? 'open' : ''}`}>
-      <ul>
-        <li><a href="#home">🏠 Home</a></li>
-        <li><a href="#adopt">🐶 Adopt</a></li>
-        <li><a href="#favorites">⭐ Favorites</a></li>
-        <li><a href="#about">📖 About</a></li>
-      </ul>
-    </div>
+    <>
+      <div className={`slideout-menu ${isOpen ? 'open' : ''}`}>
+        <ul onClick={onClose}>
+          <li><a href="#home">🏠 Home</a></li>
+          <li><a href="#adopt">🐶 Adopt</a></li>
+          <li><a href="#favorites">⭐ Favorites</a></li>
+          <li><a href="#about">📖 About</a></li>
+        </ul>
+      </div>
+      {isOpen && <div className="backdrop" onClick={onClose} />}
+    </>
   );
 };
 

5c55f7a1094526e066cfa5c36e7fe6e41a6c7ca6
Zachary Baca <zachary.baca@msn.com>
2025-06-12 08:11:24 -0500

    Update Footer.jsx

--- no-dog-left-behind/src/components/Footer/Footer.jsx
+++ no-dog-left-behind/src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
 
     return (
         <footer id="footer">
-            The Footer Component
+            <div id="tagline-container">
+                <p>&copy; {new Date().getFullYear()} Adopt. Don't shop.</p>
+            </div>
+            
+            <div className="footer-social">
+                <a href="https://facebook.com">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+                <a href="https://instagram.com">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://twitter.com">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+            </div>
+
         </footer>
     )
 }

1de2aabfef0a5040b98e35c8941b775604e8d1c5
Zachary Baca <zachary.baca@msn.com>
2025-06-12 11:59:13 -0500

    Update vite.config.js

--- no-dog-left-behind/vite.config.js
+++ no-dog-left-behind/vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // default, but good to be explicit
+    assetsDir: 'assets', // where images/fonts will go
+  },
+  server: {
+    port: 5173, // default dev port; safe to leave
+  },
+  base: '/', // change to '/your-subdir/' if deploying to a subfolder
 });

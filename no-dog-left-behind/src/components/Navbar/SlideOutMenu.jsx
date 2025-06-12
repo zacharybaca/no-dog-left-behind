c494fb3ab1fded94b8bda9774023d630cf06ae1f
Zachary Baca <zachary.baca@msn.com>
2025-06-12 10:11:56 -0500

    Update SlideOutMenu.jsx

--- no-dog-left-behind/src/components/Navbar/SlideOutMenu.jsx
+++ no-dog-left-behind/src/components/Navbar/SlideOutMenu.jsx
@@ -1,1 +1,16 @@
+import './slide-out-menu.css';
 
+const SlideOutMenu = ({ isOpen }) => {
+  return (
+    <div className={`slideout-menu ${isOpen ? 'open' : ''}`}>
+      <ul>
+        <li><a href="#home">🏠 Home</a></li>
+        <li><a href="#adopt">🐶 Adopt</a></li>
+        <li><a href="#favorites">⭐ Favorites</a></li>
+        <li><a href="#about">📖 About</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default SlideOutMenu;

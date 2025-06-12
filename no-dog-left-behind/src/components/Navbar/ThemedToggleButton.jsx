a7f7289dc5ee001c3118ac315efee8dfd96aef2a
Zachary Baca <zachary.baca@msn.com>
2025-06-12 10:12:57 -0500

    Update ThemedToggleButton.jsx

--- no-dog-left-behind/src/components/Navbar/ThemedToggleButton.jsx
+++ no-dog-left-behind/src/components/Navbar/ThemedToggleButton.jsx
@@ -1,1 +1,18 @@
+import './themed-toggle-button.css';
 
+const ThemedToggleButton = ({ menuOpen, onToggle }) => {
+  return (
+    <button
+      className={`themed-hamburger ${menuOpen ? 'open' : ''}`}
+      onClick={onToggle}
+      aria-label="Toggle navigation menu"
+      aria-expanded={menuOpen}
+    >
+      <div className="bar bar1" />
+      <div className="bar bar2" />
+      <div className="bar bar3" />
+    </button>
+  );
+};
+
+export default ThemedToggleButton;

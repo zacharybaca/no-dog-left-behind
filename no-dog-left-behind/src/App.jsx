10ab5dfb960669ed4a769cdc5a5bd0ec42e27ba8
Zachary Baca <zachary.baca@msn.com>
2025-06-12 08:53:42 -0500

    Update App.jsx

--- no-dog-left-behind/src/App.jsx
+++ no-dog-left-behind/src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
@@ -8,18 +7,20 @@ import AppLogo from './assets/no-dog-left-behind-logo.png';
 
 function App() {
 
-
   return (
     <div id="app-container">
       {/* This Section Will Show Up With Each Rendered Component */}
       <img src={AppLogo} alt="application-logo" className="application-logo" />
       <HeroSection />
-      <hr id="horizontal-ruler-default" />
+      <hr className="horizontal-ruler-default" />
 
       {/* Routes and Their Rendered Components */}
       <Routes>
         <Route exact path="/" element={<Login />} />
       </Routes>
+
+      <hr className="horizontal-ruler-default" />
+      
       <Footer />
     </div>
   )

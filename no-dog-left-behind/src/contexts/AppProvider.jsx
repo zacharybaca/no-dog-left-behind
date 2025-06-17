6de38c6f5433634cfa67e3a864b1b32a8703fe20
zacharybaca <zachary.baca@msn.com>
2025-06-16 21:06:40 -0500

    wrapped auth provider within main context file

--- no-dog-left-behind/src/contexts/AppProvider.jsx
+++ no-dog-left-behind/src/contexts/AppProvider.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./Auth/AuthProvider.jsx";
 
 export const AppProviders = ({ children }) => {
 
     return (
         <Router>
-            {children}
+            <AuthProvider>
+                {children}
+            </AuthProvider>
         </Router>
     )
 }

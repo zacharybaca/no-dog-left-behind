be10639c3df6db8f1ebb50d78d8dbcb6781ef046
Zachary Baca <zachary.baca@msn.com>
2025-06-18 12:21:18 -0500

    Update SuccessNotification.jsx

--- no-dog-left-behind/src/components/SuccessNotification/SuccessNotification.jsx
+++ no-dog-left-behind/src/components/SuccessNotification/SuccessNotification.jsx
@@ -1,59 +1,32 @@
+// SuccessNotification.jsx
 import './success-notification.css';
-import { useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { ToastContainer } from 'react-bootstrap';
 import { useNotification } from '../../hooks/useNotification';
 import SwipeableToast from '../SwipeableToast/SwipeableToast.jsx';
 
-const SuccessNotification = ({ headerText, bodyText, imgURL, variantTheme = "success", timestamp }) => {
-    const {
-        showNotification,
-        handleSwipeDismiss,
-        toggleNotification,
-        deleteNotification,
-        calculateElapsedMinutes,
-        setElapsedMinutes,
-        setNotifications,
-        notifications
-    } = useNotification();
+const SuccessNotification = () => {
+  const {
+    showNotification,
+    calculateElapsedMinutes,
+    notifications
+  } = useNotification();
 
-    useEffect(() => {
-        const newNotification = {
-            _id: uuidv4(),
-            headerText,
-            bodyText,
-            imgURL,
-            variantTheme,
-            timestamp: timestamp || Date.now()
-        };
-
-        setNotifications(prev => [...prev, newNotification]);
-
-        const interval = setInterval(() => {
-            setElapsedMinutes(calculateElapsedMinutes(newNotification.timestamp));
-        }, 60000);
-
-        return () => clearInterval(interval);
-    }, [timestamp]);
-
-    return (
-        <div id="notifications-container">
-            {notifications.length > 0 && showNotification && (
-                <ToastContainer className="p-3 custom-toast-container" position="bottom-end">
-                    <ul className="toast-list">
-                        {notifications.map(notification => (
-                            <SwipeableToast
-                                key={notification._id}
-                                deleteNotification={deleteNotification}
-                                notification={notification}
-                                calculateElapsedMinutes={calculateElapsedMinutes}
-                            />
-                        ))}
-                    </ul>
-                </ToastContainer>
-            )}
-        </div>
-    );
+  return (
+    <div id="notifications-container">
+      {notifications.length > 0 && showNotification && (
+        <ToastContainer className="p-3 custom-toast-container" position="bottom-end">
+          {notifications.map(notification => (
+            <div key={notification._id} style={{ marginBottom: '1rem' }}>
+              <SwipeableToast
+                notification={notification}
+                calculateElapsedMinutes={calculateElapsedMinutes}
+              />
+            </div>
+          ))}
+        </ToastContainer>
+      )}
+    </div>
+  );
 };
 
 export default SuccessNotification;

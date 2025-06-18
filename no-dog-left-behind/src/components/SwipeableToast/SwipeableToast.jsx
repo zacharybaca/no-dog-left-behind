b0b9f5caa001606d024f9a50a1b3b68352c69e07
Zachary Baca <zachary.baca@msn.com>
2025-06-18 12:22:59 -0500

    Update SwipeableToast.jsx

--- no-dog-left-behind/src/components/SwipeableToast/SwipeableToast.jsx
+++ no-dog-left-behind/src/components/SwipeableToast/SwipeableToast.jsx
@@ -1,46 +1,43 @@
 import { Toast } from 'react-bootstrap';
 import { useSwipeable } from 'react-swipeable';
-import { useState } from 'react';
+import { useNotification } from '../../hooks/useNotification'; 
 
-const SwipeableToast = ({ notification, deleteNotification, calculateElapsedMinutes }) => {
-  const [visible, setVisible] = useState(true);
+const SwipeableToast = ({ notification, calculateElapsedMinutes }) => {
+  const { toggleNotification } = useNotification();
 
   const swipeHandlers = useSwipeable({
-    onSwipedLeft: () => setVisible(false),
-    onSwipedRight: () => setVisible(false),
+    onSwipedLeft: () => toggleNotification(notification._id),
+    onSwipedRight: () => toggleNotification(notification._id),
     preventScrollOnSwipe: true,
     trackTouch: true,
   });
 
-  if (!visible) return null;
-
   return (
-    <li {...swipeHandlers}>
-      <Toast
-        show={visible}
-        onClose={() => deleteNotification(notification._id)} // Only deletes when "X" is clicked
-        bg={notification.variantTheme}
-        delay={3000}
-        autohide
-      >
-        <Toast.Header closeButton>
-          <img
-            src={notification.imgURL || "holder.js/20x20?text=%20"}
-            className="rounded me-2"
-            alt="toast notification"
-          />
-          <strong className="me-auto">{notification.headerText}</strong>
-          <small className="text-muted">
-            {notification.timestamp
-              ? `${calculateElapsedMinutes(notification.timestamp)} minute(s) ago`
-              : 'just now'}
-          </small>
-        </Toast.Header>
-        <Toast.Body className={notification.variantTheme === "success" ? "text-white" : ""}>
-          {notification.bodyText}
-        </Toast.Body>
-      </Toast>
-    </li>
+    <Toast
+      {...swipeHandlers}
+      show={notification.visible}
+      onClose={() => toggleNotification(notification._id)}
+      bg={notification.variantTheme}
+      delay={3000}
+      autohide
+    >
+      <Toast.Header closeButton>
+        <img
+          src={notification.imgURL || "holder.js/20x20?text=%20"}
+          className="rounded me-2"
+          alt="toast notification"
+        />
+        <strong className="me-auto">{notification.headerText}</strong>
+        <small className="text-muted">
+          {notification.timestamp
+            ? `${calculateElapsedMinutes(notification.timestamp)} minute(s) ago`
+            : 'just now'}
+        </small>
+      </Toast.Header>
+      <Toast.Body className={notification.variantTheme && "text-white"}>
+        {notification.bodyText}
+      </Toast.Body>
+    </Toast>
   );
 };
 

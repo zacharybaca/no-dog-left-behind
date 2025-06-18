767bc28cd43cfb25ea28c6f24bc8e189876e2c97
Zachary Baca <zachary.baca@msn.com>
2025-06-18 12:23:33 -0500

    Update NotificationProvider.jsx

--- no-dog-left-behind/src/contexts/Notifications/NotificationProvider.jsx
+++ no-dog-left-behind/src/contexts/Notifications/NotificationProvider.jsx
@@ -3,23 +3,32 @@ import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 
 export const NotificationProvider = ({ children }) => {
-    const [showNotification, setShowNotification] = useState(false);
+    const [showNotification, setShowNotification] = useState(true);
     const [notifications, setNotifications] = useState([{
         _id: uuidv4(),
         headerText: "Success",
         bodyText: "You Have Successfully Logged In!",
         variantTheme: "success",
-        timestamp: Date.now()
+        timestamp: Date.now(),
+        visible: true
     }, {
         _id: uuidv4(),
         headerText: "Log-In Failed!",
         bodyText: "We Were Unable to Log You Into the System.",
         variantTheme: "danger",
-        timestamp: Date.now()
+        timestamp: Date.now(),
+        visible: true
     }]);
     const [dismissed, setDismissed] = useState(false);
     const toggleDismiss = () => setDismissed(prev => !prev);
-    const toggleNotification = () => setShowNotification(prev => !prev);
+    const toggleNotification = (id) => {
+            setNotifications(prev =>
+                prev.map(n =>
+                n._id === id ? { ...n, visible: !n.visible } : n
+            )
+        );
+    };
+
     const [elapsedMinutes, setElapsedMinutes] = useState(0);
 
     const handleSwipeDismiss = (id) => {
@@ -36,19 +45,18 @@ export const NotificationProvider = ({ children }) => {
         setNotifications(prev => prev.filter(n => n._id !== id));
     };
 
-    const addNotification = ({ headerText, bodyText, imgUrl, variantTheme = "info" }) => {
-        setNotifications(prev => [
-            ...prev,
-            {
-                _id: uuidv4(),
-                headerText,
-                bodyText,
-                imgUrl,
-                variantTheme,
-                timestamp: Date.now()
-            }
-        ]);
-    };
+    const addNotification = ({ headerText, bodyText, imgURL, variantTheme = 'success' }) => {
+        const newNotification = {
+            _id: uuidv4(),
+            headerText,
+            bodyText,
+            imgURL,
+            variantTheme,
+            timestamp: Date.now(),
+            visible: true
+        };
+        setNotifications(prev => [...prev, newNotification]);
+};
 
     return (
         <NotificationContext.Provider value={{

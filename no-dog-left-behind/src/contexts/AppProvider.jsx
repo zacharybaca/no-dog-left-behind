7441c492c054f78b579d41edb27396d03569cd2d
Zachary Baca <zachary.baca@msn.com>
2025-06-19 10:58:27 -0500

    Update AppProvider.jsx

--- no-dog-left-behind/src/contexts/AppProvider.jsx
+++ no-dog-left-behind/src/contexts/AppProvider.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './Auth/AuthProvider.jsx'
 import { NotificationProvider } from './Notifications/NotificationProvider.jsx'
 import { DogSearchProvider } from './DogSearch/DogSearchProvider.jsx'
+import { MenuOptionsProvider } from './MenuOptions/MenuOptionsProvider.jsx';
 
 export const AppProviders = ({ children }) => {
   return (
     <Router>
       <AuthProvider>
-        <DogSearchProvider>
-          <NotificationProvider>{children}</NotificationProvider>
-        </DogSearchProvider>
+        <MenuOptionsProvider>
+          <DogSearchProvider>
+            <NotificationProvider>{children}</NotificationProvider>
+          </DogSearchProvider>
+        </MenuOptionsProvider>
       </AuthProvider>
     </Router>
   )

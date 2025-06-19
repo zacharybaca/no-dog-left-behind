50310c77e137d2db5fea33a8926313f80d515461
Zachary Baca <zachary.baca@msn.com>
2025-06-19 08:41:21 -0500

    Create Logout.jsx

---
+++ no-dog-left-behind/src/components/Logout/Logout.jsx
@@ -0,0 +1,13 @@
+import './log-out.css';
+import Button from 'react-bootstrap/Button';
+
+const Logout = () => {
+
+    return (
+        <Button type="button" className="logout-button">
+            <i className="bi bi-box-arrow-right me-2"></i> Logout
+        </Button>
+    )
+}
+
+export default Logout;

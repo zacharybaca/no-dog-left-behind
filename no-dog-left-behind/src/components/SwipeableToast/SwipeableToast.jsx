3a756c41be870d2771e705e57eb09e11a417cab0
Zachary Baca <zachary.baca@msn.com>
2025-06-20 10:52:01 -0500

    Update SwipeableToast.jsx

--- no-dog-left-behind/src/components/SwipeableToast/SwipeableToast.jsx
+++ no-dog-left-behind/src/components/SwipeableToast/SwipeableToast.jsx
@@ -19,6 +19,8 @@ const SwipeableToast = ({ notification, calculateElapsedMinutes }) => {
       show={notification.visible}
       onClose={() => toggleNotification(notification._id)}
       bg={notification.variantTheme}
+      delay={3000}
+      autohide
     >
       <Toast.Header closeButton>
         <img

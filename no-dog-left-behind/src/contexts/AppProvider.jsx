b882771701d19bf5ed63348cdf0fbd95bbc8a5cc
root <root@Zachs-PC.>
2025-07-05 19:51:50 -0500

    changes

--- no-dog-left-behind/src/contexts/AppProvider.jsx
+++ no-dog-left-behind/src/contexts/AppProvider.jsx
@@ -1,25 +1,24 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import { AuthProvider } from './Auth/AuthProvider.jsx'
-import { NotificationProvider } from './Notifications/NotificationProvider.jsx'
-import { DogSearchProvider } from './DogSearch/DogSearchProvider.jsx'
-import { MenuOptionsProvider } from './MenuOptions/MenuOptionsProvider.jsx'
-import { FetcherProvider } from './Fetcher/FetcherProvider.jsx'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './Auth/AuthProvider.jsx';
+import { NotificationProvider } from './Notifications/NotificationProvider.jsx';
+import { DogSearchProvider } from './DogSearch/DogSearchProvider.jsx';
+import { MenuOptionsProvider } from './MenuOptions/MenuOptionsProvider.jsx';
+import { FetcherProvider } from './Fetcher/FetcherProvider.jsx';
 
 export const AppProviders = ({ children }) => {
   return (
     <Router>
       <NotificationProvider>
-        <AuthProvider>
-          <FetcherProvider>
+        <FetcherProvider>
+          <AuthProvider>
             <MenuOptionsProvider>
               <DogSearchProvider>
                 {children}
               </DogSearchProvider>
             </MenuOptionsProvider>
-          </FetcherProvider>
-        </AuthProvider>
+          </AuthProvider>
+        </FetcherProvider>
       </NotificationProvider>
     </Router>
-
   );
 };

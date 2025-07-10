32c6cdc34c01bcd4c7d4b5a6aa8871d9d986d45b
Zachary Baca <zachary.baca@msn.com>
2025-07-10 07:44:21 -0500

    Create LoadingApplication.jsx

---
+++ no-dog-left-behind/src/components/LoadingApplication/LoadingApplication.jsx
@@ -0,0 +1,18 @@
+import './loading-application.css';
+
+const LoadingApplication = () => {
+
+    return (
+        <div id="loading-container">
+            <div id="loading-image-container">
+                Loading Image
+            </div>
+
+            <div id="text-loading-container">
+                Loading
+            </div>
+        </div>
+    )
+};
+
+export default LoadingApplication;

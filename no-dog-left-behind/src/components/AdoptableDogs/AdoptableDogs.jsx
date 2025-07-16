027d6238d3d6428bb44dd50da162ecccd34daae2
Zachary Baca <zachary.baca@msn.com>
2025-07-16 14:10:37 -0500

    Update AdoptableDogs.jsx

--- no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogs.jsx
+++ no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogs.jsx
@@ -1,11 +1,22 @@
 import './adoptable-dogs.css'
 
-const AdoptableDogs = () => {
+const AdoptableDogs = ({ dogs, dogIds }) => {
+    let dogsForAdoption = dogs.length > 0
 
     return (
-        <div>
-            Dogs
-        </div>
+        dogsForAdoption ? dogs.map((dog, id) => {
+            <div className="adoptable-dogs-container" key={dog.id}>
+                <p>`ID: ${dog.id}`</p>
+                <p>`Image of Dog: <img src={dog.img} />`</p>
+                <p>`Name of Dog: ${dog.name}`</p>
+                <p>`Age of Dog: ${dog.age}`</p>
+                <p>`Zip of Dog: ${dog.zip_code}`</p>
+                <p>`Breed of Dog: ${dog.breed}`</p>
+            </div>
+        })
+        :   <div className="adoptable-dogs-container">
+                <h1>No Adoptable Dogs Available</h1>
+            </div>
     )
 }
 

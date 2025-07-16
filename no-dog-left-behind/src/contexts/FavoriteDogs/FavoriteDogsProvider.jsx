d5275f3da2a45fc569868bbb839045f205eed865
Zachary Baca <zachary.baca@msn.com>
2025-07-16 07:21:19 -0500

    Update FavoriteDogsProvider.jsx

--- no-dog-left-behind/src/contexts/FavoriteDogs/FavoriteDogsProvider.jsx
+++ no-dog-left-behind/src/contexts/FavoriteDogs/FavoriteDogsProvider.jsx
@@ -1,1 +1,10 @@
+import { FavoriteDogsContext } from './FavoriteDogsContext.jsx'
 
+export const FavoriteDogsProvider = ({ children }) => {
+
+    return (
+        <FavoriteDogsContext.Provider>
+            {children}
+        </FavoriteDogsContext.Provider>
+    )
+}

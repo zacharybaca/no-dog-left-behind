a6830e4fbf47bd8940ef7e3681628c244e8a394b
Zachary Baca <zachary.baca@msn.com>
2025-07-16 07:34:56 -0500

    Update FavoriteDogsProvider.jsx

--- no-dog-left-behind/src/contexts/FavoriteDogs/FavoriteDogsProvider.jsx
+++ no-dog-left-behind/src/contexts/FavoriteDogs/FavoriteDogsProvider.jsx
@@ -1,9 +1,13 @@
 import { FavoriteDogsContext } from './FavoriteDogsContext.jsx'
+import { useState } from 'react'
 
 export const FavoriteDogsProvider = ({ children }) => {
+    const [favoriteDogs, setFavoriteDogs] = useState([])
 
     return (
-        <FavoriteDogsContext.Provider>
+        <FavoriteDogsContext.Provider value={{
+            favoriteDogs
+        }}>
             {children}
         </FavoriteDogsContext.Provider>
     )

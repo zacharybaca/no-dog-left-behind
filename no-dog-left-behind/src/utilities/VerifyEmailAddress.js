295624b824f03f096af8e91a3c06207e0c6ed3dc
Zachary Baca <zachary.baca@msn.com>
2025-08-06 11:17:18 -0500

    Create VerifyEmailAddress.js

---
+++ no-dog-left-behind/src/utilities/VerifyEmailAddress.js
@@ -0,0 +1,19 @@
+export async function VerifyEmailAddress(email) {
+  const apiKey = import.meta.env.VITE_EMAIL_VERIFY_API_KEY
+  const apiUrl = import.meta.env.VITE_EMAIL_VERIFY_URL
+
+  try {
+    const response = await fetch(apiUrl, { method: 'POST', headers: { 'X-API-KEY': apiKey }, body: JSON.stringify( {'email': email})} )
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+
+    const data = await response.json()
+    console.log('✅ TrueGuard verification result:', data)
+    return data
+  } catch (err) {
+    console.error('❌ Error verifying with TrueGuard:', err.message)
+    throw err
+  }
+}

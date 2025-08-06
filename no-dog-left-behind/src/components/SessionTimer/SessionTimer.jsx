2a1463fd74538769810aca8cb1fded8d7dbdfbbc
Zachary Baca <zachary.baca@msn.com>
2025-08-06 07:38:48 -0500

    Update SessionTimer.jsx

--- no-dog-left-behind/src/components/SessionTimer/SessionTimer.jsx
+++ no-dog-left-behind/src/components/SessionTimer/SessionTimer.jsx
@@ -1,13 +1,15 @@
 import './session-timer.css'
-
 import { useTimeLeft } from '../../hooks/useFormatTimeLeft'
 
 const SessionTimer = ({ expiration }) => {
   const timeRemaining = useTimeLeft(expiration)
 
+  const isExpired = timeRemaining === 'Expired'
+  const expiringSoon = !isExpired && /(\d+)m/.test(timeRemaining) && parseInt(timeRemaining.match(/(\d+)m/)[1]) <= 5
+
   return (
-    <div className="session-timer">
-      {timeRemaining === 'Expired'
+    <div className={`session-timer ${isExpired ? 'expired' : ''} ${expiringSoon ? 'expiring-soon' : ''}`}>
+      {isExpired
         ? <span className="expired-text">🔒 Session Expired</span>
         : <span className="countdown-text">⏳ Expires in: {timeRemaining}</span>}
     </div>

51dc2ee79e5e2e1404245c9f12daf96e984bbf9d
Zachary Baca <zachary.baca@msn.com>
2025-08-19 08:06:47 -0500

    Update AdoptableDogs.jsx

--- no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogs.jsx
+++ no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogs.jsx
@@ -2,33 +2,38 @@ import './adoptable-dogs.css'
 import AdoptableDogCard from './AdoptableDogCard.jsx'
 
 const AdoptableDogs = ({ dogs }) => {
-    let dogsForAdoption = dogs.length > 0
+    const dogsForAdoption = dogs.length > 0
     console.log('Dogs For Adoption: ', dogsForAdoption)
 
     return (
-        dogsForAdoption ? dogs.map((dog) => (
-            <AdoptableDogCard
-                key={dog.id}
-                Id={dog.id}
-                dogImg={dog.img}
-                dogName={dog.name}
-                dogAge={dog.age}
-                zipOfDog={dog.zip_code}
-                dogBreed={dog.breed}
-                dog={{
-                    id: `${dog.id}`,
-                    img: `${dog.img}`,
-                    name: `${dog.name}`,
-                    age: `${dog.age}`,
-                    zip_code: `${dog.zip_code}`,
-                    breed: `${dog.breed}`
-                }}
-            />
-        ))
-        :   <div className="adoptable-dogs-text-container">
-                <h1>No Adoptable Dogs Available</h1>
-            </div>
+        <div className='dogs-container'>
+            {dogsForAdoption ? (
+                dogs.map((dog) => (
+                    <AdoptableDogCard
+                        key={dog.id}
+                        Id={dog.id}
+                        dogImg={dog.img}
+                        dogName={dog.name}
+                        dogAge={dog.age}
+                        zipOfDog={dog.zip_code}
+                        dogBreed={dog.breed}
+                        dog={{
+                            id: `${dog.id}`,
+                            img: `${dog.img}`,
+                            name: `${dog.name}`,
+                            age: `${dog.age}`,
+                            zip_code: `${dog.zip_code}`,
+                            breed: `${dog.breed}`
+                        }}
+                    />
+                ))
+            ) : (
+                <div className="adoptable-dogs-text-container">
+                    <h1>No Adoptable Dogs Available</h1>
+                </div>
+            )}
+        </div>
     )
 }
 
-export default AdoptableDogs;
+export default AdoptableDogs

3f794afa1f9e8295a47498a3b20f65888f35beea
Zachary Baca <zachary.baca@msn.com>
2025-08-19 09:41:49 -0500

    Update AdoptableDogCard.jsx

--- no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogCard.jsx
+++ no-dog-left-behind/src/components/AdoptableDogs/AdoptableDogCard.jsx
@@ -7,35 +7,41 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
 
 const AdoptableDogCard = ({ Id, dogImg, dogName, dogAge, zipOfDog, dogBreed, dog }) => {
-  const { addFavoriteDog, favoritedDog, isAFavoriteDog } = useFavoriteDogs()
+  const { addFavoriteDog, isAFavoriteDog } = useFavoriteDogs()
   const aFavoriteDog = isAFavoriteDog(Id)
 
-  const handleFavoriteClick = (dog) => {
-    addFavoriteDog(dog)
+  const handleFavoriteClick = (dog, e) => {
+    addFavoriteDog(dog, e)   // ⚠ remember addFavoriteDog expects event too
   }
 
   return (
     <div className="card-wrapper">
       <Card id="custom-dog-card">
-      <Card.Img className="card-image" variant="top" src={dogImg} />
-      <Card.Body>
-        <div className="dog-name-title">
-          <Card.Title className="dog-name">Meet {dogName}</Card.Title>
-        </div>
-        <hr />
-        <Card.Text>
-          Dog ID: {Id}<br />
-          Age of Dog: {dogAge}<br />
-          ZipCode of Dog: {zipOfDog}<br />
-          Breed of Dog: {dogBreed}
-        </Card.Text>
-        <div className="card-button-container">
-          <Button variant="primary" className="favorite-button" onClick={() => handleFavoriteClick(dog)}>
-            {!favoritedDog && !aFavoriteDog ? <FontAwesomeIcon icon={faRegularHeart} size="lg" style={{ color: '#f60940' }} /> : <FontAwesomeIcon icon={faHeart} size="lg" style={{ color: "#f60940" }} />}
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+        <Card.Img className="card-image" variant="top" src={dogImg} />
+        <Card.Body>
+          <div className="dog-name-title">
+            <Card.Title className="dog-name">Meet {dogName}</Card.Title>
+          </div>
+          <hr />
+          <Card.Text>
+            Dog ID: {Id}<br />
+            Age of Dog: {dogAge}<br />
+            ZipCode of Dog: {zipOfDog}<br />
+            Breed of Dog: {dogBreed}
+          </Card.Text>
+          <div className="card-button-container">
+            <Button
+              variant="primary"
+              className="favorite-button"
+              onClick={(e) => handleFavoriteClick(dog, e)}
+            >
+              {aFavoriteDog 
+                ? <FontAwesomeIcon icon={faHeart} size="lg" style={{ color: "#f60940" }} /> 
+                : <FontAwesomeIcon icon={faRegularHeart} size="lg" style={{ color: '#f60940' }} />}
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 };

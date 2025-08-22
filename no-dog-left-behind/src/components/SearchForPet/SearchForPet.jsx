54d1c6a8ae5cb2cf2b03fc534cf8ade176ce6cc5
Zachary Baca <zachary.baca@msn.com>
2025-08-22 13:47:52 -0500

    Update SearchForPet.jsx

--- no-dog-left-behind/src/components/SearchForPet/SearchForPet.jsx
+++ no-dog-left-behind/src/components/SearchForPet/SearchForPet.jsx
@@ -2,48 +2,8 @@ import { useSearchForPet } from '../../hooks/useSearchForPet'
 import "./search-for-pet.css";
 
 const SearchForPet = () => {
-  // const [query, setQuery] = useState("");
-  // const [highlightedIndex, setHighlightedIndex] = useState(-1); // track arrow navigation
-  // const [selected, setSelected] = useState(null); // track selection
 
   const { query, setQuery, highlightedIndex, setHighlightedIndex, selected, filtered, handleKeyDown, handleSelect } = useSearchForPet()
-  // const filtered = items.filter((item) =>
-  //   item.toLowerCase().includes(query.toLowerCase())
-  // );
-
-  // const handleKeyDown = (e) => {
-  //   if (!filtered.length) return;
-
-  //   switch (e.key) {
-  //     case "ArrowDown":
-  //       setHighlightedIndex((prev) =>
-  //         prev < filtered.length - 1 ? prev + 1 : 0
-  //       );
-  //       break;
-  //     case "ArrowUp":
-  //       setHighlightedIndex((prev) =>
-  //         prev > 0 ? prev - 1 : filtered.length - 1
-  //       );
-  //       break;
-  //     case "Enter":
-  //       if (highlightedIndex >= 0) {
-  //         setSelected(filtered[highlightedIndex]);
-  //         setQuery(filtered[highlightedIndex]); // fill input with selection
-  //       }
-  //       break;
-  //     case "Escape":
-  //       setHighlightedIndex(-1);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
-
-  // const handleSelect = (item) => {
-  //   setSelected(item);
-  //   setQuery(item);
-  //   setHighlightedIndex(-1);
-  // };
 
   return (
     <div className="search-box-container">

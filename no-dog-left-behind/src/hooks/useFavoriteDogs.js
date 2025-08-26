21ff1298f596c3ac489447570a83c94a85e0a7d6
Zachary Baca <zachary.baca@msn.com>
2025-08-26 12:19:16 -0500

    Update useFavoriteDogs.js

--- no-dog-left-behind/src/hooks/useFavoriteDogs.js
+++ no-dog-left-behind/src/hooks/useFavoriteDogs.js
@@ -1,4 +1,10 @@
 import { useContext } from 'react'
 import { FavoriteDogsContext } from '../contexts/FavoriteDogs/FavoriteDogsContext'
 
-export const useFavoriteDogs = () => useContext(FavoriteDogsContext)
+export const useFavoriteDogs = () => {
+  const context = useContext(FavoriteDogsContext)
+  if (!context) {
+    throw new Error('useFavoriteDogs must be used within a FavoriteDogsProvider')
+  }
+  return context
+}

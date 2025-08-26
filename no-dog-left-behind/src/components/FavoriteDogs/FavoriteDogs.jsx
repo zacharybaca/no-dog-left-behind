1684a85490fbca51458aa94532def9961a31e40c
Zachary Baca <zachary.baca@msn.com>
2025-08-26 11:52:13 -0500

    Update FavoriteDogs.jsx

--- no-dog-left-behind/src/components/FavoriteDogs/FavoriteDogs.jsx
+++ no-dog-left-behind/src/components/FavoriteDogs/FavoriteDogs.jsx
@@ -1,15 +1,32 @@
 import './favorite-dogs.css'
 import { useFavoriteDogs } from '../../hooks/useFavoriteDogs'
 import { useDogSearch } from '../../hooks/useDogSearch'
+import AdoptableDogCard from '../AdoptableDogs/AdoptableDogCard'
+
 
 const FavoriteDogs = () => {
-  const { favoriteDogIds, favoriteMatchedDog } = useFavoriteDogs()
+  const { favoriteMatchedDog } = useFavoriteDogs()
   const { dogs } = useDogSearch()
 
+  const matchedDog = dogs.filter((dog) => dog.id === favoriteMatchedDog)[0]
+  console.log('Matched: ', matchedDog[0])
   return (
-   <div>
-    <h1>{favoriteMatchedDog}</h1>
-   </div>
+    <>
+        <div className="fav-dog-card-heading">
+            <h1>Congratulations! We Matched You With {matchedDog.name}!</h1>
+        </div>
+        <div className="favorite-dog-card">
+            <AdoptableDogCard 
+                key={matchedDog.id}
+                Id={matchedDog.id}
+                dogImg={matchedDog.img}
+                dogName={matchedDog.name}
+                dogAge={matchedDog.age}
+                zipOfDog={matchedDog.zip_code}
+                dogBreed={matchedDog.breed}
+            />
+        </div>
+    </>
   )
 }
 
